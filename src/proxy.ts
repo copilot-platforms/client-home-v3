33805d4fd870649dd92acf6fb18e715873513d5f
Rojan Rajbhandari <rrojan20@gmail.com>
2025-12-18 19:51:00 +0545

    feat(OUT-2821): use proxy.ts for authorization

---
+++ src/proxy.ts
@@ -0,0 +1,62 @@
+import { AssemblyNoTokenError } from '@assembly/errors'
+import ClientUser from '@assembly/models/client-user.model'
+import InternalUser from '@assembly/models/internal-user.model'
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import authorizedRoutes from '@/lib/auth/authorized-routes'
+import { getSanitizedHeaders } from '@/lib/auth/utils'
+import { withErrorHandler } from '@/lib/with-error-handler'
+
+/**
+ * Application proxy that handles authentication and authorization for internal and client users
+ */
+export const proxy = withErrorHandler(async (req: NextRequest) => {
+  const pathname = req.nextUrl.pathname
+  const token = req.nextUrl.searchParams.get('token')
+  if (!token) {
+    throw new AssemblyNoTokenError()
+  }
+
+  // Handle public routes
+  if (authorizedRoutes.public.includes(pathname)) {
+    return NextResponse.next()
+  }
+
+  const headers = getSanitizedHeaders(req)
+
+  // Handle IU routes
+  if (authorizedRoutes.internalUsers.includes(pathname)) {
+    const internalUser = await InternalUser.authenticate(token)
+    return NextResponse.next({
+      headers: {
+        ...headers,
+        'x-internal-user-id': internalUser.internalUserId,
+        'x-workspace-id': internalUser.workspaceId,
+      },
+    })
+  }
+
+  // Handle CU routes
+  if (authorizedRoutes.clientUsers.includes(pathname)) {
+    const client = await ClientUser.authenticate(token)
+    return NextResponse.next({
+      headers: {
+        ...headers,
+        'x-client-id': client.clientId,
+        'x-company-id': client.companyId,
+        'x-workspace-id': client.workspaceId,
+      },
+    })
+  }
+
+  // Handle unauthorized requests
+  return NextResponse.json({
+    message: 'Unauthorized',
+    status: 401,
+  })
+})
+
+export const config = {
+  // Run middleware on all routes except Next internals and static files
+  matcher: '/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)',
+}
