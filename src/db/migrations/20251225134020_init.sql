43980d0affc6c1d7d75aa0d9cb9fa8db69128690
Rojan Rajbhandari <rrojan20@gmail.com>
2025-12-25 19:28:13 +0545

    feat(OUT-2834): add initial migration

--- src/db/migrations/20251225134020_init.sql
+++ src/db/migrations/20251225134020_init.sql
@@ -1,3 +1,16 @@
+CREATE TABLE "media" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"workspace_id" varchar(16) NOT NULL,
+	"created_by_id" uuid NOT NULL,
+	"name" varchar(1028) NOT NULL,
+	"path" varchar(1028) NOT NULL,
+	"alt" varchar(1028),
+	"type" varchar(1028) NOT NULL,
+	"size" varchar(1028) NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+
 CREATE TABLE "actions" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"workspace_id" varchar(16) NOT NULL,
@@ -12,19 +25,6 @@ CREATE TABLE "actions" (
 	CONSTRAINT "uq_actions__settings_id" UNIQUE("settings_id")
 );
 
-CREATE TABLE "media" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"workspace_id" varchar(16) NOT NULL,
-	"created_by_id" uuid NOT NULL,
-	"name" varchar(1028) NOT NULL,
-	"path" varchar(1028) NOT NULL,
-	"alt" varchar(1028),
-	"type" varchar(1028) NOT NULL,
-	"size" varchar(1028) NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
-	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
-);
-
 CREATE TABLE "settings" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"workspace_id" varchar(16) NOT NULL,
@@ -37,6 +37,6 @@ CREATE TABLE "settings" (
 
 ALTER TABLE "actions" ADD CONSTRAINT "actions_settings_id_settings_id_fk" FOREIGN KEY ("settings_id") REFERENCES "public"."settings"("id") ON DELETE cascade ON UPDATE no action;
 ALTER TABLE "settings" ADD CONSTRAINT "settings_banner_image_id_media_id_fk" FOREIGN KEY ("banner_image_id") REFERENCES "public"."media"("id") ON DELETE no action ON UPDATE no action;
-CREATE INDEX "idx_actions__workspace_id" ON "actions" USING btree ("workspace_id");
 CREATE INDEX "idx_media__workspace_id" ON "media" USING btree ("workspace_id");
+CREATE INDEX "idx_actions__workspace_id" ON "actions" USING btree ("workspace_id");
 CREATE INDEX "idx_settings__workspace_id" ON "settings" USING btree ("workspace_id");
