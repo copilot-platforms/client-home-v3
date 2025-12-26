e10e83eceaf303930679d08a60bb546933caaa44
Rojan Rajbhandari <rrojan20@gmail.com>
2025-12-26 09:21:15 +0545

    refactor(OUT-2834): use new controller name

--- src/app/api/settings/route.ts
+++ src/app/api/settings/route.ts
@@ -1,4 +1,4 @@
-import { getSettings } from '@settings/lib/settings.controller'
+import { getSettingsWithActions } from '@settings/lib/settings-actions.controller'
 import { withErrorHandler } from '@/lib/with-error-handler'
 
-export const GET = withErrorHandler(getSettings)
+export const GET = withErrorHandler(getSettingsWithActions)
