39f79d0454b446ae472b1e9cf21b348388b97520
SandipBajracharya <sndp106.sb@gmail.com>
2025-12-31 14:04:35 +0545

    feat(OUT-1838): sidebar banner component

--- src/features/editor/components/Sidebar/Accordion/index.tsx
+++ src/features/editor/components/Sidebar/Accordion/index.tsx
@@ -5,7 +5,7 @@ import type { ReactNode } from 'react'
 import { cn } from '@/components/utils'
 import { useAccordion } from '@/features/editor/components/Sidebar/Accordion/useAccordion'
 
-interface AccordionProps {
+export type AccordionProps = {
   title: string
   content: ReactNode
   className?: string
@@ -18,25 +18,23 @@ export const Accordion = ({ title, content, className }: AccordionProps) => {
       <button
         type="button"
         onClick={() => setIsOpen(!isOpen)}
-        className="flex w-full items-center justify-between py-4 pr-5 pl-6 text-left focus:outline-none"
+        className="flex w-full items-center justify-between py-4 text-left focus:outline-none"
       >
-        <span className="font-medium text-gray-900">{title}</span>
+        <span className="font-medium text-gray-900 leading-6">{title}</span>
         <Icon
           icon="ChevronRight"
           width={16}
           height={16}
-          className={cn('text-text-primary transition-all duration-100 ease-in-out', isOpen ? 'rotate-90' : '')}
+          className={cn('m-1.5 text-text-primary transition-all duration-100 ease-in-out', isOpen ? 'rotate-90' : '')}
         />
       </button>
       <div
         className={cn(
-          'grid px-6 transition-all duration-100 ease-in-out',
-          isOpen ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0',
+          'grid transition-all duration-100 ease-in-out',
+          isOpen ? 'my-2 grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0',
         )}
       >
-        <div className="overflow-hidden">
-          <div className="pb-2 text-gray-600 text-sm">{content}</div>
-        </div>
+        <div className="overflow-hidden">{content}</div>
       </div>
     </div>
   )
--- src/features/editor/components/Sidebar/index.tsx
+++ src/features/editor/components/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/components/utils'
 import { Accordion } from '@/features/editor/components/Sidebar/Accordion'
+import { Banner } from '@/features/editor/components/Sidebar/Banner'
 
 interface SidebarProps {
   className?: string
@@ -8,46 +9,19 @@ interface SidebarProps {
 const AccordionItems = [
   {
     title: 'Banner',
-    content: (
-      <div className="flex flex-col gap-y-2">
-        <div className="flex items-center gap-x-2">
-          <span className="text-gray-500">Dummy banner section</span>
-          <span className="text-gray-900">Image</span>
-        </div>
-        <div className="flex items-center gap-x-2">
-          <span className="text-gray-500">Banner Color</span>
-          <span className="text-gray-900">Color</span>
-        </div>
-      </div>
-    ),
-  },
-  {
-    title: 'Actions',
-    content: 'Space for actions',
-  },
-  {
-    title: 'Dynamic fields',
-    content: 'Space for dynamic fields',
-  },
-  {
-    title: 'Background color',
-    content: 'Space for background color',
-  },
-  {
-    title: 'Segments',
-    content: 'Space for segments',
+    content: <Banner />,
   },
 ]
 
 export const Sidebar = ({ className }: SidebarProps) => {
   return (
-    <aside className={cn('h-screen border-border-gray border-l', className)}>
+    <aside className={cn('h-screen overflow-y-auto border-border-gray border-l', className)}>
       <div className="box-content flex h-14 items-center border-border-gray border-b px-6 text-custom-xl">
         Customization
       </div>
       <div className="flex flex-col">
         {AccordionItems.map((item) => (
-          <Accordion key={item.title} title={item.title} content={item.content} />
+          <Accordion key={item.title} title={item.title} content={item.content} className="pr-5 pl-6" />
         ))}
       </div>
     </aside>
